ce6156e507b6288322cde3f6fcda990d4ef79995
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-09-14 18:08:46 +0000

    00

--- Funcionario.java
+++ Funcionario.java
@@ -2,9 +2,25 @@ public class Funcionario {
     private String nome;
     private String cpf;
     private double salario;
+    private int tipo = 0;
+
+
+    public int getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(int tipo){
+        this.tipo = tipo;
+    }
 
     public double getBonificacao() {
+        if (this.tipo == 0){
         return this.salario * 0.1;
+        } else if (this.tipo == 1){
+            return this.salario * 1;
+        } else{
+            return this.salario * 2;
+        }
     }
 
     public String getNome() {
--- TestaFuncionario.java
+++ TestaFuncionario.java
@@ -4,11 +4,27 @@ public class TestaFuncionario {
         Funcionario rainhadoExpediente1 = new Funcionario();
         rainhadoExpediente1.setCpf("888.126.479-45");
         rainhadoExpediente1.setNome("Bianca");
-        rainhadoExpediente1.setSalario(3654.25);
+        rainhadoExpediente1.setSalario(20954.25);
+        rainhadoExpediente1.setTipo(2);
 
-        System.out.println(rainhadoExpediente1.getNome());
-        System.out.println(rainhadoExpediente1.getCpf());
-        System.out.println(rainhadoExpediente1.getSalario());
-        System.out.println(rainhadoExpediente1.getBonificacao());
+        System.out.println("O nome é: " + rainhadoExpediente1.getNome());
+        System.out.println("O CPF é: " + rainhadoExpediente1.getCpf());
+        System.out.println("O salário é: " + rainhadoExpediente1.getSalario());
+        System.out.println("A bonificação é: " + rainhadoExpediente1.getBonificacao());
+
+        Gerente Ana = new Gerente();
+        Ana.setCpf("369.325.145-50");
+        Ana.setNome("Ana Volgue Diniz");
+        Ana.setSalario(10);
+        Ana.setTipo(1);
+        Ana.setSenha(123456);
+
+        System.out.println("\n O nome é: " + Ana.getNome());
+        System.out.println("O CPF é: " + Ana.getCpf());
+        System.out.println("O salário é: " + Ana.getSalario());
+        System.out.println("A bonificação é: " + Ana.getBonificacao());
+        System.out.println(Ana.autentica(123456));
+
+
     }
 }
